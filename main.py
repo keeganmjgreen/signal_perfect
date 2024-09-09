1f072361ea796347adb80ea240d73c1847666695
Keegan Green <keegan.green@bluwave-ai.com>
2024-09-09 19:50:15 -0400

    formatting

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
+from typing import List, Literal
+
 import numpy as np
 import scipy as sp
 import pandas as pd
+
 pd.options.plotting.backend = "plotly"
 
-k = np.array([0, 0.5, 2, 3, 4, 5, 6.5, 7, 8])
-y = np.array([np.nan, 3, 1, 4, 1, 5, 9, 2, 6])
-n = len(k) - 1
 
-boundary_condition = "zero-curvature"
+class SpecialQuadraticSpline:
+    ppoly: sp.interpolate.PPoly
 
-A1 = np.array([[0, 0, 0] * (i-1) + [-(k[i] - k[i-1])**2, -(k[i] - k[i-1]), -1, 0, 0, 1] + [0, 0, 0] * (n-i-1) for i in range(1, n)])
-b1 = np.zeros((n-1, 1))
+    def __init__(
+        self,
+        k: List[float],
+        y: List[float],
+        boundary_condition: Literal["zero-slope", "zero-curvature"] = "zero-curvature",
+    ):
+        n = len(k) - 1
 
-A2 = np.array([[0, 0, 0] * (i-1) + [-2 * (k[i] - k[i-1]), -1, 0, 0, 1, 0] + [0, 0, 0] * (n-i-1)  for i in range(1, n)])
-b2 = np.zeros((n-1, 1))
+        A1 = np.array([[0, 0, 0] * (i-1) + [-(k[i] - k[i-1])**2, -(k[i] - k[i-1]), -1, 0, 0, 1] + [0, 0, 0] * (n-i-1) for i in range(1, n)])
+        b1 = np.zeros((n-1, 1))
 
-A3 = np.array([[0, 0, 0] * (i-1) + [2 * (k[i] - k[i-1])**3, 3 * (k[i] - k[i-1])**2, 6 * (k[i] - k[i-1])] + [0, 0, 0] * (n-i) for i in range(1, n+1)])
-b3 = np.array([[6 * y[i] * (k[i] - k[i-1])] for i in range(1, n+1)])
+        A2 = np.array([[0, 0, 0] * (i-1) + [-2 * (k[i] - k[i-1]), -1, 0, 0, 1, 0] + [0, 0, 0] * (n-i-1)  for i in range(1, n)])
+        b2 = np.zeros((n-1, 1))
 
-if boundary_condition == "zero-slope":
-    A4 = np.array([[0, 1, 0] + [0, 0, 0] * (n-1), [0, 0, 0] * (n-1) + [2 * (k[n] - k[n-1]), 1, 0]])
-elif boundary_condition == "zero-curvature":
-    A4 = np.array([[2, 0, 0] + [0, 0, 0] * (n-1), [0, 0, 0] * (n-1) + [2, 0, 0]])
-b4 = np.array([[0], [0]])
+        A3 = np.array([[0, 0, 0] * (i-1) + [2 * (k[i] - k[i-1])**3, 3 * (k[i] - k[i-1])**2, 6 * (k[i] - k[i-1])] + [0, 0, 0] * (n-i) for i in range(1, n+1)])
+        b3 = np.array([[6 * y[i] * (k[i] - k[i-1])] for i in range(1, n+1)])
 
-A = np.concat([A1, A2, A3, A4])
-b = np.concat([b1, b2, b3, b4])
+        if boundary_condition == "zero-slope":
+            A4 = np.array([[0, 1, 0] + [0, 0, 0] * (n-1), [0, 0, 0] * (n-1) + [2 * (k[n] - k[n-1]), 1, 0]])
+        elif boundary_condition == "zero-curvature":
+            A4 = np.array([[2, 0, 0] + [0, 0, 0] * (n-1), [0, 0, 0] * (n-1) + [2, 0, 0]])
+        b4 = np.array([[0], [0]])
 
-x = np.linalg.solve(A, b)
+        A = np.concat([A1, A2, A3, A4])
+        b = np.concat([b1, b2, b3, b4])
 
-ppoly = sp.interpolate.PPoly(c=x.reshape((n, 3)).T, x=k)
-xs = np.linspace(k[0], k[-1], 1000)
-ys = ppoly(xs)
-pd.Series(ys, index=xs).plot().show()#.write_image("tmp.png")
+        x = np.linalg.solve(A, b)
+
+        self.ppoly = sp.interpolate.PPoly(c=x.reshape((n, 3)).T, x=k)
+
+
+if __name__ == "__main__":
+    k = np.array([0, 0.5, 2, 3, 4, 5, 6.5, 7, 8])
+    sqs = SpecialQuadraticSpline(
+        k=k,
+        y=np.array([np.nan, 3, 1, 4, 1, 5, 9, 2, 6]),
+        boundary_condition="zero-curvature",
+    )
+    xs = np.linspace(k[0], k[-1], 1000)
+    ys = sqs.ppoly(xs)
+    pd.Series(ys, index=xs).plot().show()#.write_image("tmp.png")
