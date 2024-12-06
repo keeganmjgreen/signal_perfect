c293c7659434196c4c8837e771756a8a507a8390
Keegan Green <keegan.green@bluwave-ai.com>
2024-12-06 14:39:20 -0500

    format `main.py`

--- main.py
+++ main.py
@@ -26,19 +26,51 @@ class SpecialQuadraticSpline(sp.interpolate.PPoly):
 
         n = len(k) - 1
 
-        A1 = np.array([[0, 0, 0] * (i-1) + [-(k[i] - k[i-1])**2, -(k[i] - k[i-1]), -1, 0, 0, 1] + [0, 0, 0] * (n-i-1) for i in range(1, n)])
-        b1 = np.zeros((n-1, 1))
+        A1 = np.array(
+            [
+                [0, 0, 0] * (i - 1)
+                + [-((k[i] - k[i - 1]) ** 2), -(k[i] - k[i - 1]), -1, 0, 0, 1]
+                + [0, 0, 0] * (n - i - 1)
+                for i in range(1, n)
+            ]
+        )
+        b1 = np.zeros((n - 1, 1))
 
-        A2 = np.array([[0, 0, 0] * (i-1) + [-2 * (k[i] - k[i-1]), -1, 0, 0, 1, 0] + [0, 0, 0] * (n-i-1)  for i in range(1, n)])
-        b2 = np.zeros((n-1, 1))
+        A2 = np.array(
+            [
+                [0, 0, 0] * (i - 1)
+                + [-2 * (k[i] - k[i - 1]), -1, 0, 0, 1, 0]
+                + [0, 0, 0] * (n - i - 1)
+                for i in range(1, n)
+            ]
+        )
+        b2 = np.zeros((n - 1, 1))
 
-        A3 = np.array([[0, 0, 0] * (i-1) + [2 * (k[i] - k[i-1])**3, 3 * (k[i] - k[i-1])**2, 6 * (k[i] - k[i-1])] + [0, 0, 0] * (n-i) for i in range(1, n+1)])
-        b3 = np.array([[6 * y[i] * (k[i] - k[i-1])] for i in range(1, n+1)])
+        A3 = np.array(
+            [
+                [0, 0, 0] * (i - 1)
+                + [
+                    2 * (k[i] - k[i - 1]) ** 3,
+                    3 * (k[i] - k[i - 1]) ** 2,
+                    6 * (k[i] - k[i - 1]),
+                ]
+                + [0, 0, 0] * (n - i)
+                for i in range(1, n + 1)
+            ]
+        )
+        b3 = np.array([[6 * y[i] * (k[i] - k[i - 1])] for i in range(1, n + 1)])
 
         if boundary_condition == "zero-slope":
-            A4 = np.array([[0, 1, 0] + [0, 0, 0] * (n-1), [0, 0, 0] * (n-1) + [2 * (k[n] - k[n-1]), 1, 0]])
+            A4 = np.array(
+                [
+                    [0, 1, 0] + [0, 0, 0] * (n - 1),
+                    [0, 0, 0] * (n - 1) + [2 * (k[n] - k[n - 1]), 1, 0],
+                ]
+            )
         elif boundary_condition == "zero-curvature":
-            A4 = np.array([[2, 0, 0] + [0, 0, 0] * (n-1), [0, 0, 0] * (n-1) + [2, 0, 0]])
+            A4 = np.array(
+                [[2, 0, 0] + [0, 0, 0] * (n - 1), [0, 0, 0] * (n - 1) + [2, 0, 0]]
+            )
         b4 = np.array([[0], [0]])
 
         A = np.concat([A1, A2, A3, A4])
