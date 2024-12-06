8bdec70711ebcc17b7ca8d22ff574d772079b5ab
Keegan Green <keegan.green@bluwave-ai.com>
2024-12-06 13:20:13 -0500

    add `SpecialQuadraticSpline.plot`

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from typing import Literal
 
 import numpy as np
+import plotly.graph_objects as go
 import scipy as sp
-import pandas as pd
-
-pd.options.plotting.backend = "plotly"
-
 
 BoundaryCondition = Literal["zero-slope", "zero-curvature"]
 
@@ -51,8 +48,89 @@ class SpecialQuadraticSpline(sp.interpolate.PPoly):
 
         super().__init__(c=x.reshape((n, 3)).T, x=k)
 
-    def get_series(self, k: list[float]) -> list[float]:
-        return [self.integrate(a=k[i], b=k[i+1]) / (k[i+1] - k[i]) for i in range(len(k) - 1)]
+    def plot(
+        self,
+        include: list[Literal["input-data-series", "spline"]] = [
+            "input-data-series",
+            "spline",
+        ],
+    ) -> go.Figure:
+        fig = go.Figure()
+
+        fig.update_layout(title="SignalPerfect", plot_bgcolor="#ECEFF1")
+        fig.update_xaxes(zeroline=False, showgrid=False, showticklabels=False)
+        fig.update_yaxes(zeroline=False, showgrid=False, showticklabels=False)
+
+        if "input-data-series" in include:
+            fig.update_xaxes(
+                showticklabels=True,
+                tickmode="array",
+                tickvals=self.k,
+                ticktext=[
+                    f"<i>k</i><sub>{i}</sub> = {knot}" for i, knot in enumerate(self.k)
+                ],
+                color="#1E88E5",
+            )
+            # Plot knots:
+            for knot in self.k:
+                fig.add_vline(x=knot, line_dash="dash", line_width=0.5)
+            # Plot input data series:
+            fig.add_scatter(
+                x=self.k,
+                y=[*self.y[1:], self.y[-1]],
+                line_shape="hv",
+                mode="lines",
+                line_color="#42A5F5",
+                name="Input data series",
+                showlegend=True,
+            )
+
+        xs = np.linspace(self.k[0], self.k[-1], 1000)
+        ys = self(xs)
+        if "spline" in include:
+            # Plot special quadratic spline:
+            fig.add_scatter(
+                x=xs,
+                y=ys,
+                line_color="#EF5350",
+                name="Special quadratic spline<br>(fit to input data series)",
+                showlegend=True,
+            )
+
+        fig.update_yaxes(range=[min(ys), max(ys)])
+
+        return fig
+
+    def get_series(self, k: list[float], plot: bool = False) -> list[float]:
+        series = [
+            float(self.integrate(a=k[i], b=k[i + 1]) / (k[i + 1] - k[i]))
+            for i in range(len(k) - 1)
+        ]
+        if plot:
+            fig = self.plot(include="spline")
+            fig.add_scatter(
+                x=k,
+                y=[*series, series[-1]],
+                line_shape="hv",
+                mode="lines",
+                line_color="#66BB6A",
+                name="Output data series<br>(sampled from special quadratic spline)",
+            )
+            for i, knot in enumerate(k):
+                fig.add_vline(
+                    x=knot,
+                    line_dash="dash",
+                    line_width=0.5,
+                    label=dict(
+                        text=f"<i>k</i><sub>{i}</sub> = {knot}",
+                        font_color="#43A047",
+                        textposition="end",
+                        textangle=0,
+                        padding=3,
+                    ),
+                )
+            fig.show()
+        return series
 
 
 if __name__ == "__main__":
@@ -62,7 +140,4 @@ if __name__ == "__main__":
         y=np.array([np.nan, 3, 1, 4, 1, 5, 9, 2, 6]),
         boundary_condition="zero-curvature",
     )
-    sqs.get_series(k=[5, 6, 7, 8])
-    xs = np.linspace(k[0], k[-1], 1000)
-    ys = sqs(xs)
-    pd.Series(ys, index=xs).plot().show()#.write_image("tmp.png")
+    sqs.get_series(k=[5, 6, 7, 8], plot=True)
