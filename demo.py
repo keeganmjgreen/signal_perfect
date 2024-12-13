29cddd3b0e36832aa5623019df03919d75a8b814
Keegan Green <keegan.green@bluwave-ai.com>
2024-12-13 00:37:21 -0500

    add `demo.py`

---
+++ demo.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from special_quadratic_spline import SpecialQuadraticSpline
+
+sqs = SpecialQuadraticSpline(
+    k=np.array([0, 0.5, 2, 3, 4, 5, 6.5, 7, 8]),
+    y=np.array([3, 1, 4, 1, 5, 9, 2, 6]),
+    boundary_condition="zero-curvature",
+)
+sqs.plot(include=["input-data-series"]).write_html("docs/demo/input.html")
+sqs.plot(include=["input-data-series", "spline"]).write_html(
+    "docs/demo/input,spline.html"
+)
+
+k = [5, 6, 7, 8]
+series = sqs.get_series(k)
+sqs.plot_output_series(k, series).write_html("docs/demo/spline,output.html")
+print(f"{series = }")
